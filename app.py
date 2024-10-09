3f6884ab59897ba9f95487246b4dfc4d4ac65013
Yegor Matveyas <yogafeed@gmail.com>
2024-10-09 13:00:45 +0200

    Implement paintEvent handler

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import sys
-
+from typing import List
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter
 
+from rectangle import Rectangle
+from connection import Connection
 
 class MainWindow(QWidget):
     def __init__(
@@ -13,8 +16,21 @@ class MainWindow(QWidget):
         super().__init__()
         self.rect_height = rect_height
 
+        self.rectangles: List[Rectangle] = []
+        self.connections: List[Connection] = []
+
         self.setWindowTitle("Collision of rectangles")
         self.setGeometry(50, 50, app_width, app_height)
+
+
+    def paintEvent(self, event) -> None:
+        painter = QPainter(self)
+
+        for conn in self.connections:
+            conn.draw(painter)
+
+        for rect in self.rectangles:
+            rect.draw(painter)
 
 
 if __name__ == '__main__':
